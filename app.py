ac40408431e379b725e129ba14b4ad5058f5e4ca
lmotley <lmotley2@illinois.edu>
2017-04-19 14:42:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-	return "Suggestion Home"
+	return render_template('index.html')
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
