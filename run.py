25ae75120702943fc0a9783fded45b6477fb11e4
grshen80 <grshen80@gmail.com>
2017-04-19 19:43:09 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request, url_for
+import defs
 
 app = Flask(__name__)
 
 # loads the default query form
+
+class Document():
+	path = ""
+	link = ""
+	text =""
+
+testdoc = Document()
+testdoc.file = defs.make_filename("raw/hello.txt")
+testdoc.link = "https://testsite.com/hello.txt"
+testdoc.text = defs.generate_wot("hello", "raw/hello.txt")
+
 @app.route('/')
 def form():
 	return render_template('query.html')
@@ -11,7 +23,8 @@ def form():
 @app.route('/handler/', methods=['POST'])
 def handler():
 	query = request.form['queryPost']
-	return render_template('response.html', query=query)
+	docs = [testdoc]
+	return render_template('response.html', query=query, docs=docs)
 
 # run
 if __name__ == '__main__':
