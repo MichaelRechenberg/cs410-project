dfb4cbff27f99a830e77b0ad6874afb0f1867c89
grshen80 <grshen80@gmail.com>
2017-04-22 23:39:23 -0500

    Update defs.py

--- defs.py
+++ defs.py
@@ -9,12 +9,8 @@ the query in it
 """
 def generate_wot(query, filepath):
 
-	# get the directory of the file
-	file_dir = os.path.dirname(os.path.abspath(__file__))
-	full_filepath = os.path.join(file_dir, filepath)
-
 	window = ""
-	with open(full_filepath, 'r+') as f:
+	with open(filepath, 'r+') as f:
 		for line in f:
 			if query in line:
 				window = line;
