3c85a00bfbe9f88e3ef4f142630dcada5ea7833e
lmotley <lmotley2@illinois.edu>
2017-04-22 21:07:30 -0500

    updating window generation

--- app.py
+++ app.py
@@ -1,7 +1,115 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, send_from_directory
+import os
+import socket
+
+from PSS import PSS
+
+#Global variable to the PSS_Runner 
+pss = None 
+
+#Maximum number of results we want to return to the user
+MAX_NUM_RESULTS = 10
+
+RAW_DOCS_DIRECTORY = PSS.PSS_Runner.raw_docs_dir
+
+if os.name != "nt":
+    import fcntl
+    import struct
+
+    def get_interface_ip(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                ifname[:15]))[20:24])
+
+def get_lan_ip():
+    ip = socket.gethostbyname(socket.gethostname())
+    if ip.startswith("127.") and os.name != "nt":
+        interfaces = [
+            "eth0",
+            "eth1",
+            "eth2",
+            "wlan0",
+            "wlan1",
+            "wifi0",
+            "ath0",
+            "ath1",
+            "ppp0",
+            ]
+        for ifname in interfaces:
+            try:
+                ip = get_interface_ip(ifname)
+                break
+            except IOError:
+                pass
+    return ip
+
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def main():
-	return render_template('index.html')
+  return render_template('query.html')
+
+@app.route('/result', methods = ['POST'])
+def handler():
+  query = request.form['query']
+  #Use the PSS_Runner to score the query
+  results = pss.score_query(query, MAX_NUM_RESULTS)
+  windows = []
+  #run through result tuples and create a list of wots using the filepaths
+  for elem in results:
+    window = generate_wot(query, elem[2])
+    windows.append(window)
+  return render_template('result.html', query = query, windows = windows)
+
+def generate_wot(query, filepath):
+
+  # get the directory of the file
+  file_dir = os.path.dirname(os.path.abspath(__file__))
+  full_filepath = os.path.join(file_dir, filepath)
+
+  window = ""
+  with open(full_filepath, 'r+') as f:
+    for line in f:
+      if query in line:
+        window = line;
+        break
+
+  return window;
+
+@app.route('/raw/<path:doc_request>')
+def show_doc(doc_request):
+  #local_ip = get_lan_ip
+  return send_from_directory(RAW_DOCS_DIRECTORY, doc_request);
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+
+  print("Creating PSS_Runner object")
+
+  #Initialize the PSS_Runner
+  pss = PSS.PSS_Runner(os.getcwd(), "config.toml")
+
+  #Set parser mappings
+  parser_mappings = {
+    'header_files': PSS.copy_parser_function,
+    'mp_docs':      PSS.stupid_parse_file2,
+    'angrave_book': PSS.html_parser_function,
+    'pdfs':         PSS.pdf_parser_function
+  }
+
+  pss.parser_mappings = parser_mappings
+
+  #Set collection weights
+  weights = {
+    'header_files': 1,
+    'angrave_book': 5,
+    'pdfs'        : 1,
+    'mp_docs'     : 3,
+  }
+
+  pss.weights = weights
+  pss.parse_raw_docs()
+  pss.generate_index()
+
+  print("PSS_Runner Created Successfully")
+
+  app.run(host='0.0.0.0')
