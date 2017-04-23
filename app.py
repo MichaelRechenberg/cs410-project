ebd7a772a52e0b2f7e843a6abf233af7a2563ce5
lmotley <lmotley2@illinois.edu>
2017-04-23 00:21:29 -0500

    added filename and window functionality

--- app.py
+++ app.py
@@ -55,26 +55,28 @@ def handler():
   #Use the PSS_Runner to score the query
   results = pss.score_query(query, MAX_NUM_RESULTS)
   windows = []
+  raw_paths = []
+  file_names = []
   #run through result tuples and create a list of wots using the filepaths
   for elem in results:
-    window = generate_wot(query, elem[2])
+    window = generate_wot(query, os.path.join(pss.get_parsed_docs_dir, str(elem[0])))
     windows.append(window)
-  return render_template('result.html', query = query, windows = windows)
+    raw_paths.append(elem[2])
+    file_names.append(os.path.basename(elem[2]))
+  
+  return render_template('result.html', query = query, windows = windows, raw_paths = raw_paths, file_names = file_names)
 
 def generate_wot(query, filepath):
 
-  # get the directory of the file
-  file_dir = os.path.dirname(os.path.abspath(__file__))
-  full_filepath = os.path.join(file_dir, filepath)
-
   window = ""
-  with open(full_filepath, 'r+') as f:
+  with open(filepath, 'r+') as f:
     for line in f:
       if query in line:
         window = line;
         break
 
   return window;
+
 
 @app.route('/raw/<path:doc_request>')
 def show_doc(doc_request):
