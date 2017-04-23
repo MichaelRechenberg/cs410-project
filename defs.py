05d0a194018fe77ed7d930494767ba92d2e79346
grshen80 <grshen80@gmail.com>
2017-04-23 01:56:34 -0500

    pls

--- defs.py
+++ defs.py
@@ -17,62 +17,14 @@ def generate_wot(query, filepath):
 	with open(full_filepath, 'r+') as f:
 		for line in f:
 			if query in line:
-
-				splitLine = line.split(" ")
-				if len(splitLine) > 20:
-					#if there are more than 20 words, don't display
-					#all of them
-					window = cut_line(query, 20, line)
-				else:
-					window = line
-
+				window = line;
 				break
 
 
-	return window
-
-"""
-cuts the line around the query to n words in front and behind it
-"""
-def cut_line(query, n, line):
-	splitLines = line.split(query)
-
-	if (len(splitLines) == 2):
-		# the word appears only once
-
-		before = splitLines[0]
-		beforeWords = before.split(" ")
-		after = splitLines[1]
-		afterWords = after.split(" ")
-
-		if len(beforeWords) > n/2:
-			beforeWords = beforeWords[len(beforeWords) - n/2:]
-
-		if len(afterWords) > n/2:
-			afterWords = afterWords[:n/2]
-
-		newLine = "..."
-		for word in beforeWords[:-1]:
-			newLine += word + " "
-		newLine += beforeWords[-1]
-
-		newLine += query
-
-		for word in afterWords[:-1]:
-			newLine += word + " "
-		newLine += afterWords[-1]
-
-		newLine += "..."
-		return newLine
-
-	else:
-		# if there are multiple occurrences of query
-		# a la google style... wip lol
-
-		return line
+	return window;
 
 """
 gets the filename from the filepath
 """
 def make_filename(filepath):
-	return os.path.splitext(filepath)[0]
+	return os.path.basename(filepath)
